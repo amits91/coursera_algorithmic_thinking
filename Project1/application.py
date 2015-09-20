8223d8b78f5439083ad06895fa8f72b1a404eaa9
Amit Srivastava <srivastava.amit@gmail.com>
2015-09-20 14:33:20 +0530

    Application1: Q1, loglog plot of citation graph

--- Project1/application.py
+++ Project1/application.py
@@ -5,5 +5,15 @@ Code for Application 1
 __author__ = 'asrivas'
 import degrees
 import random_graph as rndgraph
+import parse_graph as citation
+import matplotlib.pyplot as plt
 
-print degrees.print_info(degrees.EX_GRAPH2)
+norm_in_deg_dist = degrees.norm_in_deg_dist(citation.citation_graph)
+
+plt.xlabel('in-degrees')
+plt.ylabel('Distribution')
+plt.title('LogLog graph of in-degree distribution')
+plt.loglog(norm_in_deg_dist.keys(), norm_in_deg_dist.values(), basex=2, basey=2, linestyle='None',
+           marker='x', markeredgecolor='red')
+plt.show()
+# print degrees.print_info(citation.citation_graph)
